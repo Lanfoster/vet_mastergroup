6c0f705f38a31151a137fd889c8ba7d636d77876
Cédric Bourgeois <bourgeois.lib@gmail.com>
2015-12-10 14:32:15 +1100

    parcours range (manque tableau strategy)

--- range_services/main.js
+++ range_services/main.js
@@ -6,17 +6,35 @@ define(['jquery', 'Backbone','underscore', 'transit', 'snap' ], function( $, Bac
 
       init: function( container ) {
         var that = this;
+
+        container.find(".popup .bloc").css({ opacity: 0, scale:.9 }).fadeOut();
+
         container.find( ".introBloc li").click( function() {
-          container.find(".popup").fadeIn(function(){ container.find('.'+$(this).data('link')).transition({ opacity:1, scale:1 }) });
+          var bloc = container.find('.popup .'+$(this).data('link')).eq(0);
+
+           if(bloc.length != 0) container.find(".popup").fadeIn(function(){ bloc.show().transition({ opacity:1, scale:1 }) });
         })
 
         container.find( ".popup .close").click( function() {
-          container.find(".popup .bloc").transition({ opacity:0, scale:.9 }, function(){ container.find(".popup").fadeOut() });
+          slide = 1;
+          container.find(".popup .bloc").transition({ opacity:0, scale:.9 }, function(){ container.find(".popup, .popup .bloc").fadeOut() });
         })
 
-         container.find( ".popup .zoom").click( function() {
+         container.find( ".popup .loupe").click( function() {
           window.location.hash = window.location.hash + "/viewer/protect";
         })
+
+        //****///
+
+        var slide = 1;
+        container.find('.navleft').click(function(){  slide--; slider(this); });
+        container.find('.navright').click(function(){ slide++; slider(this); });
+
+        function slider(t)
+        {
+          slide = slide > 3 ? 1 : slide < 1 ? 3 : slide;
+          $(t).parents('.bloc').transition({ opacity:0, scale:.9 }, 200, function(){ $(this).fadeOut(); container.find('.slide'+slide).show().transition({ opacity:1, scale:1 }); });
+        }
 
         //****///
 
