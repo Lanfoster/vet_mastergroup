cbd1e1facb0d792d008c1a10ede6f1c440c601e0
Cédric Bourgeois <bourgeois.lib@gmail.com>
2015-12-10 16:10:35 +1100

    Marbocyl

--- marbocyl_injectable/main.js
+++ marbocyl_injectable/main.js
@@ -7,6 +7,16 @@ define(['jquery', 'Backbone','underscore', 'transit', 'snap' ], function( $, Bac
 
     init: function( container )
     {
+      container.find('.popup .close').click(function()
+      {
+        container.find('.popup .bloc').fadeOut(function(){ container.find('.popup').fadeOut(); });
+      });
+      container.find('.popBtn').click(function()
+      {
+        var bloc = container.find('.popup .'+$(this).data('link'));
+        container.find('.popup').fadeIn(function(){ bloc.fadeIn() });
+      });
+
       container.find('h1').css( { opacity : 0, y : -50 } );
       container.find('img, ul li').css( { opacity : 0, y : 40 } );
     },
