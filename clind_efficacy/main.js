989a2e23ed5621526c705ee0d1f51896e0d01d7f
Cédric Bourgeois <bourgeois.lib@gmail.com>
2015-12-10 15:40:49 +1100

    Parcours CLINDASEPTIN

--- clind_efficacy/main.js
+++ clind_efficacy/main.js
@@ -7,10 +7,24 @@ define(['jquery', 'Backbone','underscore', 'transit', 'snap' ], function( $, Bac
 
     init: function( container )
     {
+      container.find(".popup .bloc").css({ opacity:0, scale:.9 });
+      container.find(".popBtn").click( function()
+      {
+        var bloc = container.find('.popup .'+$(this).data('popin')).eq(0);
+        if(bloc.length != 0) container.find(".popup").fadeIn(function(){ bloc.show().transition({ opacity:1, scale:1 }) });
+      });
+      container.find(".popup .close").click( function()
+      {
+        container.find(".popup .bloc").transition({ opacity:0, scale:.9 }, function(){ container.find(".popup, .popup .bloc").fadeOut() });
+      });
+
+      /****/
+
       container.find(".pageTitle").css( { opacity : 0, y: -50 } );
       container.find(".supshow").css( { opacity : 0 } );
       container.find("h2").css( { opacity : 0, x: -50 } );
       container.find(".listeplay li").css( { opacity : 0, y: 20 } );
+      container.find(".popBtn").css( { opacity: 0, scale: 0 } );
     },
 
     show: function( container )
@@ -21,6 +35,8 @@ define(['jquery', 'Backbone','underscore', 'transit', 'snap' ], function( $, Bac
       var start = 5;
       container.find("h2").each(function(){ start++; container.find(this).transition({ opacity : 1, x: 0, delay:200*start }, 700, 'ease'); });
       container.find(".listeplay li").each(function(){ start++; container.find(this).transition({ opacity : 1, y: 0, delay:150*start }, 700, 'ease'); });
+
+      container.find(".popBtn").each(function(i){ $(this).transition({ opacity:1, scale:1, delay:2000+i*400 }, 700, 'ease') });
     }
 
   }
